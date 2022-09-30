280fcf19f73eeab0ca14dca1b598db684d100b7d
Alvi <huskartp@gmail.com>
2022-09-30 11:28:41 +0300

    Initial commit

---
+++ src/main/java/com/alvi/springMvcHw3/SpringMvcHw3Application.java
@@ -0,0 +1,13 @@
+package com.alvi.springMvcHw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvi/springMvcHw3/SpringMvcHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvi.springMvcHw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
